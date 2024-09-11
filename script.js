fe5875208725dc341e8b3e994878b4a9a7a2ec73
hsv95 <henri1095@gmail.com>
2024-09-11 13:11:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -28,3 +28,26 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+
+    function toggleDropdown() {
+        dropdownMenu.classList.toggle('open');
+    }
+
+    // Event Listener nur für mobile Geräte (max-width 767px)
+    if (window.innerWidth <= 767) {
+        menuIcon.addEventListener('click', toggleDropdown);
+    }
+
+    // Falls sich die Bildschirmgröße ändert
+    window.addEventListener('resize', function() {
+        if (window.innerWidth <= 767) {
+            menuIcon.addEventListener('click', toggleDropdown);
+        } else {
+            menuIcon.removeEventListener('click', toggleDropdown);
+            dropdownMenu.classList.remove('open'); // Schließe Dropdown, wenn zu Desktop gewechselt wird
+        }
+    });
+});
